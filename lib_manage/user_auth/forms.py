21315f9a56df8f78ac1f9212e0e92c8972905a9d
sahilsolo <sahilsolomon101@gmail.com>
2024-03-09 08:11:43 +0530

    ui redesign

--- lib_manage/user_auth/forms.py
+++ lib_manage/user_auth/forms.py
@@ -11,35 +11,41 @@ class CreateUserForm(UserCreationForm):
         widgets = {
              "first_name": TextInput(
                 attrs={
+                    "id": "First name",
                     "class": "border border-solid border-slate-300 text-slate-900",
                     "placeholder": "first name",
                 }
              ),
              "last_name": TextInput(
                 attrs={
+                    "id": "Last name",
                     "class": "border border-solid border-slate-300 text-slate-900",
                     "placeholder": "last name",
                 }
              ),
             "username": TextInput(
                 attrs={
+                    "id": "Username",
                     "class": "border border-solid border-slate-300 text-slate-900",
                     "placeholder": "username",
                 }
             ),
             "email": TextInput(
                 attrs={
+                    "id": "Email address",
                     "class": "border border-solid border-slate-300 text-slate-900",
                     "placeholder": "youremail@somemail.com",
                 }
             ),
             "password1": TextInput(
                 attrs={
+                    "id": "Password",
                     "class": "border border-solid border-slate-300 text-slate-900",
                 }
             ),
             "password2": TextInput(
                 attrs={
+                    "id": "Password confirmation",
                     "class": "border border-solid border-slate-300 text-slate-900",
                 }
             ),
@@ -48,9 +54,11 @@ class CreateUserForm(UserCreationForm):
 class LoginForm(AuthenticationForm):
 
     username = forms.CharField(widget = TextInput(attrs={
+                "id": "Username",
                 "class": "border border-solid border-slate-300 text-slate-900",
                 "placeholder": "username",
             }))
     password = forms.CharField(widget = PasswordInput(attrs={
+                "id": "Password",
                 "class": "border border-solid border-slate-300 text-slate-900",
             }))
