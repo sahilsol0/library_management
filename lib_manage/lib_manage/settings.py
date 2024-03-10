f15e9e3a76a732c30c8e88625ba3cc1e47c18167
sahilsolo <sahilsolomon101@gmail.com>
2024-03-10 15:05:13 +0530

    bug fixes

--- lib_manage/lib_manage/settings.py
+++ lib_manage/lib_manage/settings.py
@@ -116,8 +116,9 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_HOST_PORT = 465
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'libre.com'
-EMAIL_HOST_PASSWORD = 'libre123'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'sahilsolodev@gmail.com'
+EMAIL_HOST_PASSWORD = 'zhwu tysa kwki ecgt'
--- lib_manage/user_auth/forms.py
+++ lib_manage/user_auth/forms.py
@@ -3,6 +3,8 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, Pass
 from django import forms
 from django.forms.widgets import PasswordInput,TextInput
 
+form_style = "bg-color60 w-full text-xl p-1 rounded"
+
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
@@ -12,53 +14,65 @@ class CreateUserForm(UserCreationForm):
              "first_name": TextInput(
                 attrs={
                     "id": "First name",
-                    "class": "border border-solid border-slate-300 text-slate-900",
+                    "class": form_style,
                     "placeholder": "first name",
                 }
              ),
              "last_name": TextInput(
                 attrs={
                     "id": "Last name",
-                    "class": "border border-solid border-slate-300 text-slate-900",
+                    "class": form_style,
                     "placeholder": "last name",
                 }
              ),
             "username": TextInput(
                 attrs={
                     "id": "Username",
-                    "class": "border border-solid border-slate-300 text-slate-900",
+                    "class": form_style,
                     "placeholder": "username",
                 }
             ),
             "email": TextInput(
                 attrs={
                     "id": "Email address",
-                    "class": "border border-solid border-slate-300 text-slate-900",
+                    "class": form_style,
                     "placeholder": "youremail@somemail.com",
                 }
             ),
             "password1": TextInput(
                 attrs={
                     "id": "Password",
-                    "class": "border border-solid border-slate-300 text-slate-900",
                 }
             ),
             "password2": TextInput(
                 attrs={
                     "id": "Password confirmation",
-                    "class": "border border-solid border-slate-300 text-slate-900",
                 }
             ),
         }
-
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["password1"].widget.attrs.update({
+            "class": form_style,
+        })
+        self.fields["password2"].widget.attrs.update({
+           "class": form_style,
+        })
+    
 class LoginForm(AuthenticationForm):
 
     username = forms.CharField(widget = TextInput(attrs={
                 "id": "Username",
-                "class": "border border-solid border-slate-300 text-slate-900",
+                "class": form_style,
                 "placeholder": "username",
             }))
     password = forms.CharField(widget = PasswordInput(attrs={
                 "id": "Password",
-                "class": "border border-solid border-slate-300 text-slate-900",
+                "class": form_style,
             }))
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["password"].widget.attrs.update({
+           "class": form_style,
+        })
